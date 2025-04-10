277bf3e939fb3d29b16a39d2f3539329b1f75940
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-04-10 17:48:19 +0000

    40 import

---
+++ Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0, threes = 0;
+        
+        for (int num : nums) {
+            // twos will hold the bits that appear twice
+            twos |= ones & num;
+            
+            // ones will hold the bits that appear once
+            ones ^= num;
+            
+            // threes will hold the bits that appear three times
+            threes = ones & twos;
+            
+            // Remove the bits that appeared three times from ones and twos
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        
+        return ones;
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] nums1 = {6, 1, 3, 3, 3, 6, 6};
+        System.out.println(solution.singleNumber(nums1));  // Output: 1
+        
+        int[] nums2 = {13, 19, 13, 13};
+        System.out.println(solution.singleNumber(nums2));  // Output: 19
+    }
+}
