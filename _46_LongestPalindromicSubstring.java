a0fb163d064559fb89c9511ee6ed1ab4d5e36e1f
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-04-16 16:48:26 +0000

    #

--- _46_LongestPalindromicSubstring.java
+++ _46_LongestPalindromicSubstring.java
@@ -1,3 +1,7 @@
+// Given a string, find the longest palindromic contiguous substring. If there are more than one with the maximum length, return any one.
+
+// For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The longest palindromic substring of "bananas" is "anana".
+
 public class _46_LongestPalindromicSubstring {
     
     public static String longestPalindrome(String s) {
