bb2143c9cc45ee438877b7102f0a287e0938ad49
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-04-17 17:58:50 +0000

    47Done

--- _47_StockProfit.java
+++ _47_StockProfit.java
@@ -1,7 +1,7 @@
 // Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
 
 // For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
-public class _47StockProfit {
+public class _47_StockProfit {
     public static int maxProfit(int[] prices) {
         if (prices == null || prices.length < 2) {
             return 0; // Not enough data to make a transaction
