bbc4a1df268d8c793251eccf19411dc8c597eedc
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-05-13 18:56:46 +0000

    #

---
+++ Solution.java
@@ -0,0 +1,27 @@
+// Given the head of a singly linked list, reverse it in-place.
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        
+        while (current != null) {
+            ListNode nextTemp = current.next; // Store the next node
+            current.next = prev;               // Reverse the current node's pointer
+            prev = current;                    // Move prev and current one step forward
+            current = nextTemp;
+        }
+        
+        return prev; // prev will be the new head of the reversed list
+    }
+}
