fc8d8c75e9396731bab2ff56bb5c980416bc9e3e
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-05-18 18:22:53 +0000

    #

--- _78_MergeKSortedLists.java
+++ _78_MergeKSortedLists.java
@@ -1,3 +1,4 @@
+// Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list. 
 import java.util.PriorityQueue;
 
 public class _78_MergeKSortedLists {
