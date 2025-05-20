35de9937d7a3b79b5c25711792ae98d5f6905ae1
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-05-20 18:57:09 +0000

    #

---
+++ DeepestNodeFinder.java
@@ -0,0 +1,50 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    char val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(char x) {
+        val = x;
+    }
+}
+
+public class DeepestNodeFinder {
+    public TreeNode findDeepestNode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode deepestNode = null;
+
+        while (!queue.isEmpty()) {
+            deepestNode = queue.poll(); // Get the front node
+
+            // Add left and right children to the queue
+            if (deepestNode.left != null) {
+                queue.offer(deepestNode.left);
+            }
+            if (deepestNode.right != null) {
+                queue.offer(deepestNode.right);
+            }
+        }
+
+        return deepestNode; // The last node processed is the deepest node
+    }
+
+    public static void main(String[] args) {
+        // Construct the tree
+        TreeNode root = new TreeNode('a');
+        root.left = new TreeNode('b');
+        root.right = new TreeNode('c');
+        root.left.left = new TreeNode('d');
+
+        DeepestNodeFinder finder = new DeepestNodeFinder();
+        TreeNode deepestNode = finder.findDeepestNode(root);
+        System.out.println("The deepest node is: " + deepestNode.val); // Output: d
+    }
+}
