e249edcbc71d4352bba65a88287b4d34d329c1b0
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-06-05 18:12:45 +0000

    96 Done

--- _96_Permutations.java
+++ _96_Permutations.java
@@ -7,7 +7,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Permutations {
+public class _96_Permutations {
     public static void main(String[] args) {
         int[] digits = {1, 2, 3};
         List<List<Integer>> result = permute(digits);
