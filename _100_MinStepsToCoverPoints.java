1a6be34e10a253d6b4bc6bf1db84afb489e584eb
Ashu-cmmd <abhishekjena1590@gmail.com>
2025-06-09 18:22:23 +0000

    100 Done

--- _100_MinStepsToCoverPoints.java
+++ _100_MinStepsToCoverPoints.java
@@ -19,12 +19,12 @@
 // Output: 2
 // It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).  
 
-public class MinStepsToCoverPoints {
+public class _100_MinStepsToCoverPoints {
     public static int minSteps(int[][] points) {
         int totalSteps = 0;
-        for(int i = 1; i &lt; points.length; i++) {
-            int dx = Math.abs(points[i][0] - points[i-1][0]);
-            int dy = Math.abs(points[i][1] - points[i-1][1]);
+        for (int i = 1; i < points.length; i++) {
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
             totalSteps += Math.max(dx, dy);
         }
         return totalSteps;
